3e278b4d8aafedf9262329a48658f30d255c578c
data-datum <noelia0618@gmail.com>
2018-10-12 10:33:18 -0300

    archivos con corridas de PCA

---
+++ import.R
@@ -0,0 +1,9 @@
+#dalex for classification with random forest
+library(breakDown)
+cruz<-read.csv("G:/DATASETS/data-lap.txt")
+View(cruz)
+
+cr<-read.csv("C:/Program Files (x86)/R/CruzipainaML/out.csv", sep="\t")
+
+lig<-read.csv("C:/Program Files (x86)/R/CruzipainaML/lig_01.csv")
+
